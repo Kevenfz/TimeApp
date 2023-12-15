b98b659b52ab0b58ab57975c5f1c2afbbdeee605
Keven Ferreira Moraes <55964670+Kevenfz@users.noreply.github.com>
2023-12-14 23:46:53 -0300

    feat: Criação do SearchComponent

--- time-app/src/App.jsx
+++ time-app/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import { SearchComponent } from "./components/SearchComponent";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <h1>Olá Mundo! Criarei agora meu time app</h1>
-      <h2>testezinho</h2>
+      <SearchComponent />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
