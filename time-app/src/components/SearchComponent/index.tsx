bccd373b2bf267a9fadcbb9a459cceeff25201ea
Keven Ferreira Moraes <55964670+Kevenfz@users.noreply.github.com>
2024-02-05 21:38:50 -0300

    feat: iniciando o componente de busca

--- time-app/src/components/SearchComponent/index.tsx
+++ time-app/src/components/SearchComponent/index.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import "./style.css";
 
 export const SearchComponent = () => {
+  const [cidade, setCidade] = useState("");
+  const Search = () => {};
+
+  console.log();
+
   return (
-    <div>SearchComponent</div>
-  )
-}
+    <section>
+      <div className="HeaderSpace">
+        <div className="SearchField">
+          <input placeholder="Digite a cidade" name="sm" id="sm" />
+          <button onClick={Search} />
+        </div>
+      </div>
+    </section>
+  );
+};
